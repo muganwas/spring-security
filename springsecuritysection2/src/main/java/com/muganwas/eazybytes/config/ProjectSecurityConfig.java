8b3ddbeff850b5850747977d4de8a60845dde641
Muganwa <stevenmuganwa@live.com>
2024-06-28 15:27:33 +0300

    custom SecurityFilterChain

--- springsecuritysection2/src/main/java/com/muganwas/eazybytes/config/ProjectSecurityConfig.java
+++ springsecuritysection2/src/main/java/com/muganwas/eazybytes/config/ProjectSecurityConfig.java
@@ -1,9 +1,7 @@
 package com.muganwas.eazybytes.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,9 +10,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class ProjectSecurityConfig {
     @Bean
-    @Order(SecurityProperties.BASIC_AUTH_ORDER)
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards")
+                .authenticated()
+                .requestMatchers("/contact", "/notices").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();

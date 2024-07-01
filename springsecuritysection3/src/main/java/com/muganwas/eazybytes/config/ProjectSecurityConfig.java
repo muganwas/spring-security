f100db275840292e61ce0d552afb3dd462b83386
Muganwa <stevenmuganwa@live.com>
2024-07-01 10:12:52 +0300

    in memory users withDefaultPasswordEncoder

--- springsecuritysection3/src/main/java/com/muganwas/eazybytes/config/ProjectSecurityConfig.java
+++ springsecuritysection3/src/main/java/com/muganwas/eazybytes/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package com.muganwas.eazybytes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,11 +15,25 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer -> configurer
-                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards")
+                .requestMatchers("/myAccounts", "/myBalance", "/myLoans", "/myCards")
                 .authenticated()
                 .requestMatchers("/contact", "/notices").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("Japheth")
+                .password("dev1234.")
+                .authorities("admin")
+                .build();
+        UserDetails user_1 = User.withDefaultPasswordEncoder()
+                .username("Cleo")
+                .password("dev1234.")
+                .authorities("user")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user_1);
     }
 }
